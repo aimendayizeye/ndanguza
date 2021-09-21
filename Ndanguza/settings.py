3e7f5bc0d022c541c06ad1885d013c93f2db10cb
Memento <salimalsalif@gmail.com>
2021-09-21 18:55:43 +0200

    Added cart model

--- Ndanguza/settings.py
+++ Ndanguza/settings.py
@@ -38,12 +38,37 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'corsheaders',
     'NdanguzaApi.apps.NdanguzaapiConfig',
 ]
+REST_FRAMEWORK ={
+    
+        'DEFAULT_AUTHENTICATION_CLASSES':[
+            'rest_framework.authentication.TokenAuthentication'
+        ],
+    
+}
+CORS_ALLOWED_ORIGINS = [
+    
+    "http://localhost:3000",
+    "http://127.0.0.1:8000",
+]
+
+CORS_ALLOW_METHOD = [
+"GET",
+"POST",
+"PUT",
+"PATCH",
+
+]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
